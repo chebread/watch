3195f73c2a9e7099ca30b2131728dc4957256af8
차한음 <ihaneum@chahan-eum-ui-MacBookAir.local>
2021-07-07 00:04:34 +0900

    v3.0

--- VERSIONS.md
+++
@@ -1,7 +0,0 @@
-# Versions
-
-### v1.0
-bascis functions
-
-### v2.0
-Edit text colors, Add functions of `watch`
--- exec.py
+++ exec.py
@@ -1,14 +1,9 @@
 # watch on terminal
 from datetime import *
 from clint.textui import *
-import sys
-import os
-import os.path
+# global variable
 am_pm = 0
-def Isfile(f):
-    if os.path.isfile(f):
-        return 1
-    return -1
+# functions
 def Am(h):
     global am_pm
     if h > 12:
@@ -25,62 +20,18 @@ def Am(h):
 def Weekdays(n):
     list = ['월', '화', '수', '목', '금', '토', '일']
     return list[n]
-def LeapYear(y):
-    if (y % 4 == 0 and y % 100 != 0) or y % 400 == 0:
-        return 1
-    return -1
 def Time_12():
     global am_pm
-    print(colored.cyan("%s요일"%(Weekdays(datetime.now().today().weekday()))))
+    print(colored.blue("%s요일"%\
+    (Weekdays(datetime.now().today().weekday()))), "☁️")
     hour = Am(datetime.now().hour)
     if am_pm == 1:
-        print(colored.red('오후 '), end='')
+        print(colored.blue('오후 '), end='')
     else:
-        print(colored.red('오전 '), end='')
-    print(colored.blue("%s시 %s분 %s초"%(\
-    hour, datetime.now().minute, datetime.now().second)))
-    print(colored.green("%s년"%datetime.now().year), colored.magenta("%s월"%datetime.now().month), colored.magenta("%s일"%datetime.now().day))
+        print(colored.blue('오전 '), end='')
+    print(colored.cyan("%s시"%hour), colored.cyan("%s분"%datetime.now().minute), colored.cyan("%s초"%(datetime.now().second)))
+    print(colored.blue("%s년"%datetime.now().year), colored.cyan("%s월"%datetime.now().month), colored.cyan("%s일"%datetime.now().day))
 def Print():
-    print(colored.yellow("시계"))
     Time_12()
-def NowDir():
-    return os.path.dirname(os.path.abspath(__file__))
-def ManVer():
-    now = NowDir()
-    ver_file = now + "/doc/version.md"
-    isfile = Isfile(ver_file)
-    if isfile == -1:
-        print("Error: No version.md file")
-        return -1
-    file = open(ver_file, "rb")
-    load = file.read()
-    file.close()
-    print(colored.green(load.decode(encoding="utf-8")[:5]) + ' ' + load.decode(encoding="utf-8")[6:]) # Prints
-def ManHelp():
-    ManVer()
-    now = NowDir()
-    help_file = now + "/doc/help.md"
-    isfile = Isfile(help_file)
-    if isfile == -1:
-        print("Error: No help.md file")
-        return -1
-    file = open(help_file, "rb")
-    load = file.read()
-    file.close()
-    print(load.decode(encoding="utf-8"))
-def Flags(f):
-    for i in range(1, 3):
-        if i == 1:
-            f = f.lower()
-        if i == 2:
-            f = f.upper()
-        if (f.find('-h')==0 or f.find('--h')==0):
-            ManHelp()
-            sys.exit(0)
-        if (f.find('-v')==0 or f.find('--v')==0):
-            ManVer()
-            sys.exit(0)
-try:
-    Flags(sys.argv[1])
-except IndexError:
-    Print()
+
+Print()
